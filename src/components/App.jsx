9a2817a0266a5fe2ad852fb6807b7e51d443c10c
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-04-18 21:55:46 -0400

    Implemented Unsplash API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,24 @@ import axios from "axios"
 import SearchBar from "./SearchBar"
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term)
+  state = { images: [] }
+
+  onSearchSubmit = async (term) => {
+    const res = await axios.get("https://api.unsplash.com/search/photos", {
+      headers: {
+        Authorization: "Client-ID LPkKS9sJA6gPBuqieuXCcHZUd0FZhkKB3fAM0XyUTL8"
+      },
+      params: {
+        query: term
+      }
+    })
+    this.setState({ images: res.data.results })
   }
   render() {
     return (
       <div className="ui container " style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     )
   }
